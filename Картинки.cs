426e296cfd65494c3e8ebc089065c04b518da359
Russian-Kaiser <114874856+Russian-Kaiser@users.noreply.github.com>
2023-06-22 17:44:49 +0300

    Add files via upload

---
+++ Картинки.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countPictures = 52;
+            int remainingPictures;
+            int sizeRow = 3;
+            int filledRows;
+
+            filledRows = countPictures / sizeRow;
+            remainingPictures = countPictures - (sizeRow * filledRows);
+            Console.WriteLine($"Заполненных рядов {filledRows}.\nОсталось картинок {remainingPictures}.");
+            Console.ReadKey();
+        }
+    }
+}
