53df31a16c9bcba0d464fdff6de6a5ae32f085f9
Russian-Kaiser <114874856+Russian-Kaiser@users.noreply.github.com>
2023-06-22 17:20:58 +0300

    Add files via upload

---
+++ Работа со строками.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет?");
+            string age = Console.ReadLine();
+            Console.WriteLine("Кем вы работаете?");
+            string work = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"Здравствуйте, {name}. Вам {age} лет и вы работаете {work}.");
+            Console.ReadKey();
+        }
+    }
+}
