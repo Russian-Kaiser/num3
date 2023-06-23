f2b06337c586dfb765943b24d28ca289b86a43a8
Russian-Kaiser <114874856+Russian-Kaiser@users.noreply.github.com>
2023-06-23 20:48:20 +0300

    Add files via upload

---
+++ Магазин кристаллов.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pricePerCrystal = 15;   
+
+            Console.Write("Добро пожаловать в магазин кристаллов! \nСколько у вас имеется золота? - ");
+            int amountOfGold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nПрекрасно! Сегодня вы можете купить кристаллы по сниженной цене:");
+            Console.WriteLine($"1 кристалл - {pricePerCrystal} золотых монет.");
+            Console.Write("\nСколько вы хотите купить кристаллов? - ");
+            int amountOfСrystals = Convert.ToInt32(Console.ReadLine());
+            amountOfGold -= pricePerCrystal * amountOfСrystals;
+            Console.WriteLine($"\nВы получаете {amountOfСrystals} кристаллов. \nУ вас остаётся {amountOfGold} золотых.");
+            Console.WriteLine("Хорошего дня!");
+            Console.ReadKey();
+        }
+    }
+}
