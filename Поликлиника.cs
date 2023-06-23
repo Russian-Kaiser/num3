26e15a45833fa0c5538925742176a29e6e98fb3e
Russian-Kaiser <114874856+Russian-Kaiser@users.noreply.github.com>
2023-06-23 20:47:20 +0300

    Add files via upload

---
+++ Поликлиника.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ergrh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int receptionTimeInMinutes = 10;
+            int hourInMinutes = 60;
+
+            Console.WriteLine("Вы заходите в поликлинику и увидели очередь..");
+            Console.Write("Сколько в ней было человек? - ");
+            int queueLength = Convert.ToInt32(Console.ReadLine());
+            int waitingHours = (receptionTimeInMinutes * queueLength) / hourInMinutes;
+            int waitingMinutes = (queueLength * receptionTimeInMinutes) - (waitingHours * hourInMinutes);
+            Console.WriteLine($"Вам предстоит ожидать {waitingHours} час и {waitingMinutes} минут");
+            Console.ReadKey();
+        }
+    }
+}
