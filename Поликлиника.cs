0425d95d75cdac6016001c5bbcedc772d9c1919a
Russian-Kaiser <114874856+Russian-Kaiser@users.noreply.github.com>
2023-06-29 14:42:25 +0300

    Update Поликлиника.cs

--- Поликлиника.cs
+++ Поликлиника.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,13 +11,13 @@ namespace Поликлиника
         static void Main(string[] args)
         {
             int minuteAdmissionTime = 10;
-            int hourInMinutes = 60;
+            int minutuesInHour = 60;
 
             Console.Write("Вы заходите в коридор, сколько человек вы увидели? - ");
             int queueLength = Convert.ToInt32(Console.ReadLine());
-            int minutesOfWaiting = minuteAdmissionTime * queueLength;
-            int hoursOfWaiting = minutesOfWaiting / hourInMinutes;
-            minutesOfWaiting = minutesOfWaiting % hourInMinutes;
+            int timeOfWaiting = minuteAdmissionTime * queueLength;
+            int hoursOfWaiting = timeOfWaiting / minutuesInHour;
+            int minutesOfWaiting = timeOfWaiting % minutuesInHour;
             Console.WriteLine($"Время ожидания: {hoursOfWaiting} час и {minutesOfWaiting} минут");
             Console.ReadKey();
         }
