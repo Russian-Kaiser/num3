9e5312485bd78e2020dd016b76e5ab2d9865731a
Russian-Kaiser <114874856+Russian-Kaiser@users.noreply.github.com>
2023-07-23 14:51:12 +0300

    Add files via upload

---
+++ Степени двойки.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Степени_двойки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(2, 900);
+            int maxDegree = 11;
+            int newNumber = 0;
+            int number = 2;
+            int degree = 0;
+
+            for (int i = 0; i < maxDegree; i++)
+            {
+                degree = i;
+                newNumber = Convert.ToInt32(Math.Pow(number, degree));
+
+                if (newNumber >= randomNumber)
+                    break;
+            }
+
+            Console.WriteLine($"Рандомное число: {randomNumber}");
+            Console.WriteLine($"Полученное число - {newNumber}. Начальное число {number} в степени {degree}");
+            Console.ReadKey();
+        }
+    }
+}
