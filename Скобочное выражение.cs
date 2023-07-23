6556724fd0e377fbbcb77c2ca45c30c8ed8c2a8b
Russian-Kaiser <114874856+Russian-Kaiser@users.noreply.github.com>
2023-07-23 14:51:43 +0300

    Add files via upload

---
+++ Скобочное выражение.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Скобочное_выражение
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int nestingDepth = 0;
+            Console.Write("Введите выражение из символов '(' и ')' - ");
+            string line = Console.ReadLine();
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '(')
+                    counter++;
+
+                else if (line[i] == ')')
+                {
+                    if (i < line.Length)
+                    {
+                        i++;
+
+                        if (line[i - 1] != '(')
+                            nestingDepth++;
+
+                        i--;
+                    }
+                    counter--;
+                }
+
+                if (counter < 0)
+                    break;
+            }
+
+            if (counter == 0)
+                Console.Write($"Выражение корректно! \nМаксимальная глубина {nestingDepth - 1}");
+
+            else
+                Console.Write("Выражение некорректно!");
+
+            Console.ReadKey();
+        }
+    }
+}
