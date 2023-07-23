901de30afb49539b3dec527c4e67bd201bb509be
Russian-Kaiser <114874856+Russian-Kaiser@users.noreply.github.com>
2023-07-23 14:53:02 +0300

    Add files via upload

---
+++ Последовательность.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Последовательность
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startValue = 5;
+            int endValue = 96;
+            int step = 7;
+
+            for (int i = startValue; i <= endValue; i += step)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
