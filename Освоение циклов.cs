b0607315ccf3d2b60edd80320f610e3dd00cbb5c
Russian-Kaiser <114874856+Russian-Kaiser@users.noreply.github.com>
2023-07-23 14:53:37 +0300

    Add files via upload

---
+++ Освоение циклов.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Освоение_циклов
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сообщение:");
+            string userInput = Console.ReadLine();
+            Console.Write("Введите число повторений:");
+            int numberOfRepetitions = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberOfRepetitions; i++)
+            {
+                Console.WriteLine(userInput);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
