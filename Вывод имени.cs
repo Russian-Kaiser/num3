f8f7eb2b347e20a1b119f7e836b7a8879d2d8240
Russian-Kaiser <114874856+Russian-Kaiser@users.noreply.github.com>
2023-07-23 14:49:23 +0300

    Add files via upload

---
+++ Вывод имени.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Вывод_имени
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите символ:");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            name = symbol + name + symbol;
+            string symbolsLine = "";
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                symbolsLine += symbol;
+            }
+
+            Console.WriteLine("\n" + symbolsLine);
+            Console.WriteLine(name);
+            Console.WriteLine(symbolsLine);
+            Console.ReadKey();
+        }
+    }
+}
