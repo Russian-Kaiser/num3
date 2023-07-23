18ecda29b988e53381547fb6661ad012aa339bf0
Russian-Kaiser <114874856+Russian-Kaiser@users.noreply.github.com>
2023-07-23 14:54:29 +0300

    Add files via upload

---
+++ Контроль выхода.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Контроль_выхода
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            bool exit = true;
+
+            while(exit)
+            {
+                Console.Clear();
+                Console.Write("Введите 'exit', чтобы закончить цикл: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == "exit")
+                    exit = false;
+
+                else
+                {
+                    Console.WriteLine("Неправильное значение");
+                    Console.ReadKey();
+                } 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
