f32e0a09b201fa61f46a3ccafefd998c9fe8f096
Russian-Kaiser <114874856+Russian-Kaiser@users.noreply.github.com>
2023-07-24 20:52:31 +0300

    Скобочное выражение.cs

--- Скобочное выражение.cs
+++ Скобочное выражение.cs
@@ -12,35 +12,46 @@ namespace Скобочное_выражение
         {
             int counter = 0;
             int nestingDepth = 0;
+            int currentDepth = 0;
+            char start = '(';
+            char end = ')';
             Console.Write("Введите выражение из символов '(' и ')' - ");
             string line = Console.ReadLine();
 
             for (int i = 0; i < line.Length; i++)
             {
-                if (line[i] == '(')
-                    counter++;
+                currentDepth = i;
 
-                else if (line[i] == ')')
+                if (line[i] == start)
+                {
+                    counter++;
+                }
+                else if (line[i] == end)
                 {
                     if (i < line.Length)
                     {
-                        i++;
+                        currentDepth++;
 
-                        if (line[i - 1] != '(')
+                        if (line[currentDepth - 1] != start)
+                        {
                             nestingDepth++;
-
-                        i--;
+                        }
+                           
+                        currentDepth--;
                     }
                     counter--;
                 }
 
                 if (counter < 0)
+                {
                     break;
+                }
             }
 
-            if (counter == 0)
-                Console.Write($"Выражение корректно! \nМаксимальная глубина {nestingDepth - 1}");
+            nestingDepth = nestingDepth - 1;
 
+            if (counter == 0)
+                Console.Write($"Выражение корректно! \nМаксимальная глубина {nestingDepth}");
             else
                 Console.Write("Выражение некорректно!");
 
