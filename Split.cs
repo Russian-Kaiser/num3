67dc149a5f5930750e1f0cc2f5e65b4711df9f08
Russian-Kaiser <114874856+Russian-Kaiser@users.noreply.github.com>
2023-07-28 19:19:46 +0300

    Add files via upload

---
+++ Split.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст:");
+            string text = Console.ReadLine();
+            string [] words = text.Split(' ');
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
